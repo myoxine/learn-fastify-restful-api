fd0fe7a275b803e527e8b7b33dc246191fbbc55d
Hadi Halim <hdi_hlim@yahoo.com>
2024-08-29 16:56:14 +0700

    Menangani Request dengan Query Parameters

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,22 @@ server.get("/ping", async (request, reply) => {
   return "pong\n";
 });
 
+server.get('/search', async (request, reply) => {
+    // Ambil query parameters dari request
+    const { name, age } = request.query as { name?: string; age?: string }
+  
+    // Respons berdasarkan query parameters
+    if (name && age) {
+      return `Searching for Name: ${name} and Age: ${age}`
+    } else if (name) {
+      return `Searching for Name: ${name}`
+    } else if (age) {
+      return `Searching for Age: ${age}`
+    } else {
+      return 'No query parameters provided'
+    }
+  })
+
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
     console.error(err);
