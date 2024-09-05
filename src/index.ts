8d8ebc6e7c4218d7481f76a8dd9ec28053b417ec
Hadi Halim <hdi_hlim@yahoo.com>
2024-09-05 11:31:52 +0700

    Cara Melakukan Validasi Request di Fastify

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,32 @@ import fastify from "fastify";
 import { userRoutes } from "./routes/userRoutes";
 import { authRoutes } from "./routes/authRoutes";
 
-const server = fastify();
+const server = fastify({
+  ajv: {
+    customOptions: {
+      allErrors: true,
+    },
+    plugins: [require("ajv-errors")],
+  },
+});
+server.setErrorHandler(function (error, request, reply) {
+  if (error.validation) {
+    return reply.status(400).send({
+      path: request.url,
+      status: error.statusCode,
+      timestamp: Date.now(),
+      message: error.message,
+      errors: error.validation.map((err) => ({
+        key: err.params?.missingProperty,
+        value: err.message,
+      })),
+    });
+  }
+  reply.status(500).send(error);
+});
 
-server.register(userRoutes, {prefix:"/users"});
-server.register(authRoutes, {prefix:"/auth"});
+server.register(userRoutes, { prefix: "/users" });
+server.register(authRoutes, { prefix: "/auth" });
 
 server.get("/ping", async (request, reply) => {
   return "pong\n";
