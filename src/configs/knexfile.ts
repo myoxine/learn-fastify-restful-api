734a20e9c1c3e750a4fa6e986f1d02f5b36d80b0
Hadi Halim <hdi_hlim@yahoo.com>
2024-10-04 10:38:20 +0700

    fix lost cause merge

--- src/configs/knexfile.ts
+++ src/configs/knexfile.ts
@@ -16,10 +16,10 @@ const config: { [key: string]: Knex.Config } = {
     },
     migrations: {
       tableName: "knex_migrations",
-      directory: './migrations'
+      directory: './../migrations'
     },
     seeds: {
-      directory: './seeds'
+      directory: './../seeds'
     }
   },
   staging: {
